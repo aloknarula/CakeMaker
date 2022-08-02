a9136f8f375888dc6610cfc08b30568d8a1d06c3
aloknarula <aloknarula@outlook.com>
2022-08-02 16:15:33 +0530

    Frist commit

---
+++ CakeMakerSource/Assets/_CakeMaker/Scripts/CakeDecorationManagerUI.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CakeDecorationManagerUI : MonoBehaviour
+{
+    // Public //
+    public CakeDecorationManager m_decoManager;
+    public GameObject m_rotationPanel;
+    public GameObject m_colorPanel;
+    // Protected //
+    // Private //
+    // Access //
+
+    void Start()
+    {
+        
+    }
+
+    public void RotationY(float by)
+    {
+        if (m_decoManager.m_selectedCheez == null)
+        {
+            return;
+        }
+
+        if(m_decoManager.m_selectedCheez.m_canRotate == false)
+        {
+            return;
+        }
+
+        m_decoManager.m_selectedCheez.transform.rotation *= Quaternion.AngleAxis(by, Vector3.up);
+            //.Rotate(m_decoManager.m_selectedCheez.transform.up, by);
+
+    }
+
+    public void ColorChanged(Color changedTo)
+    {
+        if (m_decoManager.m_selectedCheez == null)
+        {
+            return;
+        }
+
+        if(m_decoManager.m_selectedCheez.m_canChangeColor == false)
+        {
+            return;
+        }
+
+        m_decoManager.m_selectedCheez.m_mainMaterial.color = changedTo;
+    }
+
+    void Update()
+    {
+        if(m_decoManager.m_selectedCheez == null)
+        {
+            m_rotationPanel.SetActive(false);
+            m_colorPanel.SetActive(false);
+        }
+        else
+        {
+            if(m_decoManager.m_selectedCheez.m_canRotate)
+            {
+                m_rotationPanel.SetActive(true);
+            }
+            else
+            {
+                m_rotationPanel.SetActive(false);
+            }
+            
+            if(m_decoManager.m_selectedCheez.m_canChangeColor)
+            {
+                m_colorPanel.SetActive(true);
+            }
+            else
+            {
+                m_colorPanel.SetActive(false);
+            }
+        }
+    }
+}
